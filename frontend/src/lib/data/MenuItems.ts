d9dc6914f34111a10e42cb3e7f96e737a59ddfb8
Nikhil John <nikhiljohn1010@gmail.com>
2023-02-27 03:38:58 +0530

    Fixed item list

--- frontend/src/lib/data/MenuItems.ts
+++ frontend/src/lib/data/MenuItems.ts
@@ -3,30 +3,36 @@ export const items = [
 		id: 1,
 		name: 'Item 1',
 		price: 45,
-		imageurl: '/images/image1.avif'
+		imageurl: '/images/image1.png'
 	},
 	{
 		id: 2,
 		name: 'Item 2',
 		price: 63,
-		imageurl: '/images/image2.avif'
+		imageurl: '/images/image2.png'
 	},
 	{
 		id: 3,
 		name: 'Item 3',
 		price: 52,
-		imageurl: '/images/image3.avif'
+		imageurl: '/images/image3.png'
 	},
 	{
 		id: 4,
 		name: 'Item 4',
 		price: 95,
-		imageurl: '/images/image4.avif'
+		imageurl: '/images/image4.png'
 	},
 	{
 		id: 5,
 		name: 'Item 5',
 		price: 74,
-		imageurl: '/images/image5.avif'
+		imageurl: '/images/image5.png'
+	},
+	{
+		id: 6,
+		name: 'Item 6',
+		price: 39,
+		imageurl: '/images/image6.png'
 	}
 ];
