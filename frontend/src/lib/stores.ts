063731c26b91c63d3d611984c8cd7de5d7de29b1
Nikhil John <nikhiljohn1010@gmail.com>
2023-02-28 00:46:25 +0530

    Fixed store

--- frontend/src/lib/stores.ts
+++ frontend/src/lib/stores.ts
@@ -1,5 +1,6 @@
 import { writable } from 'svelte/store';
 import { items } from '$data/MenuItems';
+import type { NavMenu, MenuItem } from '$ui-types';
 
-export const pageTracker = writable('home');
-export const itemCollection = writable(items);
+export const pageTracker = writable({ name: 'Home', url: '/' } as NavMenu);
+export const itemCollection = writable(items as MenuItem[]);
