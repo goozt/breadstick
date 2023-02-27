a1afea8b57954cc6dc1f1de50e7021e89258a3dc
Nikhil John <nikhiljohn1010@gmail.com>
2023-02-28 00:46:46 +0530

    updated dashboard and added settings page

---
+++ frontend/src/routes/dashboard/+page.svelte
@@ -0,0 +1,77 @@
+<script lang="ts">
+	import { pageTracker, itemCollection } from '$store';
+	import { Grid, Item } from '$ui/menu';
+	import type { MenuItem } from '$ui/types';
+	import { SpeedDial } from '$ui';
+	import { SpeedDialButton } from 'flowbite-svelte';
+	$pageTracker = { name: 'Dashboard', url: 'dashboard' };
+
+	const removeItem = (event: Event) => {
+		const e = event.target as Element;
+		const id = e.getAttribute('data-id');
+		if (id != null) {
+			const items: MenuItem[] = $itemCollection;
+			const item = items.find((item) => item.id === parseInt(id));
+			if (item) {
+				const index = items.indexOf(item);
+				if (index !== -1) {
+					items.splice(index, 1);
+				}
+				$itemCollection = items;
+				console.log('Removed item', id, 'successfully');
+			}
+		}
+	};
+</script>
+
+<div class="md:container md:mx-auto py-16 px-4">
+	<Grid>
+		{#each $itemCollection as item}
+			<Item
+				id={String(item.id)}
+				name={item.name}
+				price={item.price}
+				image={item.imageurl}
+				on:click={removeItem}>Remove</Item
+			>
+		{/each}
+	</Grid>
+</div>
+
+<SpeedDial>
+	<SpeedDialButton name="Add&nbsp;Menu&nbsp;Item" tooltip="left">
+		<svg
+			width="75%"
+			height="75%"
+			aria-hidden="true"
+			fill="currentColor"
+			viewBox="0 0 20 20"
+			xmlns="http://www.w3.org/2000/svg"
+		>
+			<path
+				clip-rule="evenodd"
+				d="M4.5 2A1.5 1.5 0 003 3.5v13A1.5 1.5 0 004.5 18h11a1.5 1.5 0 001.5-1.5V7.621a1.5 1.5 0 00-.44-1.06l-4.12-4.122A1.5 1.5 0 0011.378 2H4.5zM10 8a.75.75 0 01.75.75v1.5h1.5a.75.75 0 010 1.5h-1.5v1.5a.75.75 0 01-1.5 0v-1.5h-1.5a.75.75 0 010-1.5h1.5v-1.5A.75.75 0 0110 8z"
+				fill-rule="evenodd"
+			/>
+		</svg>
+	</SpeedDialButton>
+	<SpeedDialButton name="Settings" tooltip="left">
+		<svg
+			width="60%"
+			height="60%"
+			aria-hidden="true"
+			fill="currentColor"
+			viewBox="0 0 20 20"
+			xmlns="http://www.w3.org/2000/svg"
+		>
+			<path
+				clip-rule="evenodd"
+				d="M14.5 10a4.5 4.5 0 004.284-5.882c-.105-.324-.51-.391-.752-.15L15.34 6.66a.454.454 0 01-.493.11 3.01 3.01 0 01-1.618-1.616.455.455 0 01.11-.494l2.694-2.692c.24-.241.174-.647-.15-.752a4.5 4.5 0 00-5.873 4.575c.055.873-.128 1.808-.8 2.368l-7.23 6.024a2.724 2.724 0 103.837 3.837l6.024-7.23c.56-.672 1.495-.855 2.368-.8.096.007.193.01.291.01zM5 16a1 1 0 11-2 0 1 1 0 012 0z"
+				fill-rule="evenodd"
+			/>
+			<path
+				d="M14.5 11.5c.173 0 .345-.007.514-.022l3.754 3.754a2.5 2.5 0 01-3.536 3.536l-4.41-4.41 2.172-2.607c.052-.063.147-.138.342-.196.202-.06.469-.087.777-.067.128.008.257.012.387.012zM6 4.586l2.33 2.33a.452.452 0 01-.08.09L6.8 8.214 4.586 6H3.309a.5.5 0 01-.447-.276l-1.7-3.402a.5.5 0 01.093-.577l.49-.49a.5.5 0 01.577-.094l3.402 1.7A.5.5 0 016 3.31v1.277z"
+			/>
+		</svg>
+	</SpeedDialButton>
+</SpeedDial>
---
+++ frontend/src/routes/dashboard/settings/+page.svelte
@@ -0,0 +1,68 @@
+<script lang="ts">
+	import { Tabs, TabItem, Timeline, TimelineItem, Button } from 'flowbite-svelte';
+	import { pageTracker } from '$store';
+	$pageTracker = { name: 'Settings', url: '/dashboard/settings' };
+</script>
+
+<div class="md:container md:mx-auto py-16 px-4">
+	<Tabs>
+		<TabItem open>
+			<span slot="title">General</span>
+			<Timeline>
+				<TimelineItem title="Application UI code in Tailwind CSS" date="February 2022">
+					<p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
+						Get access to over 20+ pages including a dashboard layout, charts, kanban board,
+						calendar, and pre-order E-commerce & Marketing pages.
+					</p>
+					<Button color="alternative"
+						>Learn more
+						<svg
+							class="ml-2 w-3 h-3"
+							fill="currentColor"
+							viewBox="0 0 20 20"
+							xmlns="http://www.w3.org/2000/svg"
+							><path
+								fill-rule="evenodd"
+								d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
+								clip-rule="evenodd"
+							/></svg
+						>
+					</Button>
+				</TimelineItem>
+				<TimelineItem title="Application UI code in Tailwind CSS" date="March 2022">
+					<p class="text-base font-normal text-gray-500 dark:text-gray-400">
+						All of the pages and components are first designed in Figma and we keep a parity between
+						the two versions even as we update the project.
+					</p>
+				</TimelineItem>
+				<TimelineItem title="Application UI code in Tailwind CSS" date="April 2022">
+					<p class="text-base font-normal text-gray-500 dark:text-gray-400">
+						Get started with dozens of web components and interactive elements built on top of
+						Tailwind CSS.
+					</p>
+				</TimelineItem>
+			</Timeline>
+		</TabItem>
+		<TabItem>
+			<span slot="title">Users</span>
+			<p class="text-sm text-gray-500 dark:text-gray-400">
+				<b>Users:</b> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+				incididunt ut labore et dolore magna aliqua.
+			</p>
+		</TabItem>
+		<TabItem>
+			<span slot="title">Dashboard</span>
+			<p class="text-sm text-gray-500 dark:text-gray-400">
+				<b>Dashboard:</b> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+				incididunt ut labore et dolore magna aliqua.
+			</p>
+		</TabItem>
+		<TabItem>
+			<span slot="title">Advance</span>
+			<p class="text-sm text-gray-500 dark:text-gray-400">
+				<b>Settings:</b> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+				incididunt ut labore et dolore magna aliqua.
+			</p>
+		</TabItem>
+	</Tabs>
+</div>
